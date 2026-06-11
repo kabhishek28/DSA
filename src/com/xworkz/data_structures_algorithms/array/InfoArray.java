2743d7a4d6d4445363f9740a6edb478beb554f45
kabhishek28 <kabhishek.eng@gmail.com>
2026-06-12 00:42:03 +0530

    arrya

---
+++ src/com/xworkz/data_structures_algorithms/array/InfoArray.java
@@ -0,0 +1,15 @@
+package com.xworkz.data_structures_algorithms.array;
+
+import java.util.Objects;
+
+public class InfoArray {
+    public static void main(String[] args) {
+        String[] arrya = {"Deksheeth","Amith","Lalu"};
+        arrya[0] = "Abhi";
+        System.out.println(arrya);
+
+
+        Raja raja = new Raja();
+        System.out.println(raja instanceof Object);
+    }
+}
